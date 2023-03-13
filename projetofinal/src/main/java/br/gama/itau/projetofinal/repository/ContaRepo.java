19962e9e8498abbba0497b9d368d6ea7924b15d8
LuizGustavo <lsilveira1528@yahoo.com>
2023-03-13 15:35:19 -0300

    ContaService Adicionado conta service

---
+++ projetofinal/src/main/java/br/gama/itau/projetofinal/repository/ContaRepo.java
@@ -0,0 +1,12 @@
+package br.gama.itau.projetofinal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.gama.itau.projetofinal.model.Conta;
+
+public interface ContaRepo extends CrudRepository<Conta, Integer> {
+
+    
+} 
+    
+
