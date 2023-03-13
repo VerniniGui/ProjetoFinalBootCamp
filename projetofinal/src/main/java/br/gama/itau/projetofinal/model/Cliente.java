97e7a15f117b26ffb81264e9c74fc820aa1b6984
verniniGui <verninigui91@gmail.com>
2023-03-13 17:41:19 -0300

    Adicionada a relação Cliente x Conta

--- projetofinal/src/main/java/br/gama/itau/projetofinal/model/Cliente.java
+++ projetofinal/src/main/java/br/gama/itau/projetofinal/model/Cliente.java
@@ -1,11 +1,15 @@
 package br.gama.itau.projetofinal.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,7 +32,8 @@ public class Cliente {
     @Column (length = 20, unique = true)
     private String telefone;
 
-    // @OneToMany
-    // private List<Conta> listaContas;
+    @OneToMany(mappedBy = "idCliente")
+    @JsonIgnoreProperties("id_cliente")
+    private List<Conta> listaContas;
 
 }
