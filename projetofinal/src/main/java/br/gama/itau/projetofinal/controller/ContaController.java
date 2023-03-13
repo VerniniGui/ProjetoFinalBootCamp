eb801d13aed6e358b7fded67045502a86f10b2a7
Thiago de Almeida Freitas <106921302+Thiago-Freitas@users.noreply.github.com>
2023-03-13 16:10:14 -0300

    Conta Controller

---
+++ projetofinal/src/main/java/br/gama/itau/projetofinal/controller/ContaController.java
@@ -0,0 +1,34 @@
+package br.gama.itau.projetofinal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gama.itau.projetofinal.model.Conta;
+import br.gama.itau.projetofinal.service.ContaService;
+
+@RestController
+@RequestMapping("/contas")
+
+public class ContaController {
+    @Autowired
+    private ContaService service;
+
+    @GetMapping("/contas/{id}")
+    public ResponseEntity <Conta> recuperaPeloNumero (@PathVariable Integer id) {
+        return ResponseEntity.ok(service.recuperarPeloNumero(id));
+    }
+
+    @GetMapping("/contas/clientes/{id}")
+    public ResponseEntity <List<Conta>> recuperarconta (@PathVariable Integer id) {
+        //List<Conta> list = service.recuperarContasPeloCliente(id);
+        return ResponseEntity.ok().build();
+    }
+    
+}
--- projetofinal/src/main/java/br/gama/itau/projetofinal/service/ContaService.java
+++ projetofinal/src/main/java/br/gama/itau/projetofinal/service/ContaService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.gama.itau.projetofinal.model.Cliente;
 import br.gama.itau.projetofinal.model.Conta;
 import br.gama.itau.projetofinal.repository.ContaRepo;
 
