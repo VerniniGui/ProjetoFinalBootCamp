9c435d5aea8294fd4ff099a0077d158128911180
verniniGui <verninigui91@gmail.com>
2023-03-14 09:35:53 -0300

    Alterado o usuário do banco de dados

--- projetofinal/src/main/java/br/gama/itau/projetofinal/controller/ContaController.java
+++ projetofinal/src/main/java/br/gama/itau/projetofinal/controller/ContaController.java
@@ -3,7 +3,6 @@ package br.gama.itau.projetofinal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
