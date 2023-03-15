a02b1d4f4af4b46da0ca3f520d2897a41aa7685d
verniniGui <verninigui91@gmail.com>
2023-03-15 11:18:56 -0300

    Alteração das regras da classe cliente alterada a regra de cadastro de cliente

---
+++ projetofinal/src/main/java/br/gama/itau/projetofinal/exception/HandlerExceptions.java
@@ -0,0 +1,24 @@
+package br.gama.itau.projetofinal.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class HandlerExceptions {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionDetails> handlerNotFoundException(DataIntegrityViolationException ex) {
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .titulo("Erro na solicitação")
+                .mensagem(ex.getMessage())
+                .codigoStatus(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
+    }
+}
--- projetofinal/src/main/java/br/gama/itau/projetofinal/service/ClienteService.java
+++ projetofinal/src/main/java/br/gama/itau/projetofinal/service/ClienteService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import br.gama.itau.projetofinal.dto.ClienteDto;
+import br.gama.itau.projetofinal.exception.DataIntegrityViolationException;
 import br.gama.itau.projetofinal.model.Cliente;
 import br.gama.itau.projetofinal.model.Conta;
 import br.gama.itau.projetofinal.repository.ClienteRepo;
@@ -16,14 +18,27 @@ import br.gama.itau.projetofinal.repository.ClienteRepo;
 public class ClienteService {
     @Autowired
     private ClienteRepo repo;
-    
-    public Cliente cadastrarCliente(Cliente cliente){
+
+    public Cliente cadastrarCliente(Cliente cliente) {
+
         Cliente novoCliente = cliente;
-        repo.save(novoCliente);
+        
+
+        if (cliente.getNomeCliente() == null) {
+            cliente.setNomeCliente("Nome não cadastado");
+        }
+        
+        try {
+            repo.save(novoCliente);
+        } catch (Exception e) {
+            throw new DataIntegrityViolationException("Dados Invalidos");
+        }   
+        
         return novoCliente;
+
     };
 
-    public List<ClienteDto> recuperarTodos(){
+    public List<ClienteDto> recuperarTodos() {
         List<Cliente> list = (List<Cliente>) repo.findAll();
         List<ClienteDto> lClienteDTO = new ArrayList<>();
 
@@ -37,12 +52,12 @@ public class ClienteService {
         return lClienteDTO;
     };
 
-    public ClienteDto recuperarPeloId(int id){
+    public ClienteDto recuperarPeloId(int id) {
         Optional<Cliente> optional = repo.findById(id);
 
-        if(optional.isEmpty()){
+        if (optional.isEmpty()) {
             return null;
-        }       
+        }
         Cliente cliente = optional.get();
         ClienteDto clienteDto = new ClienteDto(cliente);
         return clienteDto;
@@ -57,6 +72,4 @@ public class ClienteService {
 
     }
 
-
-    
 }
