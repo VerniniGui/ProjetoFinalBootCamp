19942f078da6ce42cb8115307e4de19ccb4ee43b
verniniGui <verninigui91@gmail.com>
2023-03-20 16:29:59 -0300

    Alterada a varial do GenerateConta

--- projetofinal/src/test/java/br/gama/itau/projetofinal/util/GenerateConta.java
+++ projetofinal/src/test/java/br/gama/itau/projetofinal/util/GenerateConta.java
@@ -21,7 +21,7 @@ public class GenerateConta {
                 .id(1)
                 .agencia("1234")
                 .conta(1)
-                .saldo(200)
+                .saldo(100)
                 .idCliente(GenerateCliente.clienteValido())
                 .build();
     }
